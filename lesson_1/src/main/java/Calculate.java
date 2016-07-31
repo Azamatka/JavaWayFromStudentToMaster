5c9cfa5c6298cd96262c5ea8d8765270b872f02c
Azamatka <mmmzzzxz@gmail.com>
2016-07-31 17:17:41 +0500

    IAR

--- lesson_1/src/main/java/Calculate.java
+++ lesson_1/src/main/java/Calculate.java
@@ -1,5 +1,29 @@
 public class Calculate{
+	double value1=4;
+	double value2=2;
+	static double resault;
 public static void main(String[] args){
-System.out.println("Hello World");
+	Calculate calc= new Calculate();
+calc.multiply(resault);
+calc.minus(resault);
+calc.plus(resault);
+calc.divide(resault);
 }
+public   void multiply(double resault){
+	resault=(value1*value2); 
+	System.out.println(resault);
+}
+public void minus(double resault){
+		resault=(value1-value2);
+		System.out.println(resault);
+}
+public void plus(double resault){
+		resault=(value1+value2);
+		System.out.println(resault);
+}
+public void divide(double resault){
+		resault=(value1/value2);
+		System.out.println(resault);
+
 } 
+}
