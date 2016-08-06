77161137898d3731057ca8679c3769b8a481c31a
Azamatka <mmmzzzxz@gmail.com>
2016-08-06 23:33:09 +0500

    IAR

--- lesson_1/src/main/java/Calculator.java
+++ lesson_1/src/main/java/Calculator.java
@@ -1,21 +1,50 @@
+/**
+* Calculator класс для вычисления арифмитеческих операций: + - *  и /
+* @author Azamat
+* @since 6.08.2016
+* @version 1
+*/
 public class Calculator{
 	  double result;
-
+/**
+* Умножение
+* @param first первый аргумент
+* @param second второй аргумент
+* @param result записываем результат
+*/
 	public   void add(double first, double second){
 		double mult=first*second; 
 		result=mult;
 	
 	}
+	/**
+* Вычитание
+* @param first первый аргумент
+* @param second второй аргумент
+* @param result записываем результат
+*/
 	public void add1 (double first, double second){
 			double min=first-second;
 			result=min;
 			
 	}
+		/**
+* Сложение
+* @param first первый аргумент
+* @param second второй аргумент
+* @param result записываем результат
+*/
 	public void add2(double first, double second){
 			double plus=first+second;
 			result=plus;
 			
 	}
+		/**
+* Деление
+* @param first первый аргумент
+* @param second второй аргумент
+* @param result записываем результат
+*/
 	public void add3(double first, double second){
 			double div=first/second;
             result=div;
--- lesson_1/src/test/java/CalculatorTest.java
+++ lesson_1/src/test/java/CalculatorTest.java
@@ -3,28 +3,57 @@ import org.junit.Test;
 
 import static org.hamcrest.core.Is.is;
 import static org.junit.Assert.*;
-public class CalculatorTest {
+/**
+* CalculatorTest класс для проверки вычисления арифмитеческих операций: + - *  и /
+* @author Azamat
+* @since 6.08.2016
+* @version 1
+*/
+public class CalculatorTest{
+    /**
+     *  @param calc новый объект класса Calculator
+     */
+    Calculator calc=new Calculator();
+/**
+* Умножение
+* @param calc новый объект класса Calculator
+* @param add прописываем входные параметры в новом объекте класса
+* @param Assert проверяем  правильно ли работает наш метод add
+*/
     @Test
     public void WhenMultiplyShouldMyltiply(){
-        Calculator calc=new Calculator();
         calc.add(8,4);
         Assert.assertThat(calc.result, is(32.0));
     }
+	/**
+* Вычитание
+* @param calc новый объект класса Calculator
+* @param add прописываем входные параметры в новом объекте класса
+* @param Assert проверяем  правильно ли работает наш метод add1
+*/
     @Test
     public void WhenMinusShouldMinus(){
-        Calculator calc=new Calculator();
         calc.add1(8,4);
         Assert.assertThat(calc.result, is(4.0));
     }
+		/**
+* Сложение
+* @param calc новый объект класса Calculator
+* @param add прописываем входные параметры в новом объекте класса
+* @param Assert проверяем  правильно ли работает наш метод add2
+*/
     @Test
     public void WhenPlusShouldPlus(){
-        Calculator calc=new Calculator();
         calc.add2(8,4);
         Assert.assertThat(calc.result, is(12.0));
     }
+	/** Деление
+* @param calc новый объект класса Calculator
+* @param add прописываем входные параметры в новом объекте класса
+* @param Assert проверяем  правильно ли работает наш метод add3
+*/
     @Test
     public void WhenDivideShouldDivide(){
-        Calculator calc=new Calculator();
         calc.add3(8,4);
         Assert.assertThat(calc.result, is(2.0));
     }
