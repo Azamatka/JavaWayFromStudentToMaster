b4ea92c9805f252989c921961d5af2d3939f4cd9
Azamatka <mmmzzzxz@gmail.com>
2017-02-13 00:14:34 +0500

    IAR

--- lesson_2/src/main/java/Chess/Board.java
+++ lesson_2/src/main/java/Chess/Board.java
@@ -7,6 +7,7 @@ import java.util.Arrays;
  */
 public class Board {
     static Cell[][] board = new Cell[8][8];
+    static Cell[][] boardWay = new Cell[8][8];
     static Figure[] figures = new Figure[3];
 
     static class Bishop extends Figure {
@@ -22,110 +23,100 @@ public class Board {
         }
 
         public Cell[][] way(Cell dist) throws ImposibleMoveException, FigureNotFoundException, OccupiedWayException {
-
+            for (int i = 0; i < 8; i++) {
+                for (int c = 0; c < 8; c++) {
+                    board[i][c] = new Cell("Empty", i, c);
+                }
+            }
             if (dist.getDiagonal() < 8 && dist.getVertical() < 8) {
                 if (position.getDiagonal() != dist.getDiagonal() && position.getVertical() != dist.getVertical()) {
-                    if (position.getDiagonal() > dist.getDiagonal() && position.getVertical() > dist.getVertical()) {
+                    if (position.getDiagonal() > dist.getDiagonal() && position.getVertical() > dist.getVertical()){
                         for (int i = 0; i < 8; i++) {
                             if (position.getVertical() - i < 0) {
                                 throw new ImposibleMoveException("Impossible move exception");
                             }
                             int b = position.getDiagonal() - i;
                             int d = position.getVertical() - i;
-                            board[b][d] = new Cell(null, position.getDiagonal() - i, position.getVertical() - i);
-                            if (b == dist.getDiagonal() && d == dist.getVertical()) {
-                                for (Cell[] cell : board) {
-                                    for (Cell cel : cell) {
-                                        if (cel == null) {
-                                            board[b][d] = new Cell("Bishop", b, d);
-                                            return board;
-                                        } else throw new OccupiedWayException("Occupied Way Exception");
-                                    }
+                            if (board[b][d].getFigure().equals("Empty")) {
+                                boardWay[b][d] = new Cell("Empty", position.getDiagonal() - i, position.getVertical() - i);
+                                if (b == dist.getDiagonal() && d == dist.getVertical()) {
+                                    boardWay[b][d] = new Cell("Bishop", b, d);
+                                    return boardWay;
                                 }
+                            } else {
+                                throw new OccupiedWayException("Occupied Way Exception");
                             }
                         }
-
                     }
-
 
                     if (position.getDiagonal() > dist.getDiagonal() && position.getVertical() < dist.getVertical()) {
                         for (int i = 0; i < 8; i++) {
                             if (position.getDiagonal() - i < 0 || position.getVertical() > 7) {
-                                break;
+                                throw new ImposibleMoveException("Impossible move exception");
                             }
                             int b = position.getDiagonal() - i;
                             int d = position.getVertical() + i;
-                            board[b][d] = new Cell(null, position.getDiagonal() - i, position.getVertical() + i);
-                            if (b == dist.getDiagonal() && d == dist.getVertical()) {
-                                for (Cell[] cell : board) {
-                                    for (Cell cel : cell) {
-                                        if (cel == null) {
-                                            board[b][d] = new Cell("Bishop", b, d);
-                                            return board;
-                                        } else throw new OccupiedWayException("Occupied Way Exception");
-                                    }
+                           if (board[b][d].getFigure().equals("Empty")) {
+                                boardWay[b][d] = new Cell("Empty", position.getDiagonal() - i, position.getVertical() + i);
+                                if (b == dist.getDiagonal() && d == dist.getVertical()) {
+                                    boardWay[b][d] = new Cell("Bishop", b, d);
+                                    return boardWay;
+
                                 }
+                            } else {
+                                throw new OccupiedWayException("Occupied Way Exception");
                             }
                         }
-
                     }
+
                     if (position.getDiagonal() < dist.getDiagonal() && position.getVertical() < dist.getVertical()) {
-                        for (int i = 1; i < 7; i++) {
+                        for (int i = 0; i < 8; i++) {
                             if (position.getDiagonal() + i > 7) {
                                 throw new ImposibleMoveException("Impossible Move Exception");
                             }
                             int b = position.getDiagonal() + i;
                             int d = position.getVertical() + i;
-                            if (board[b][d].getFigure()!= null &&  board[b][d].getFigure().length()!=0) {
+                            if (board[b][d].getFigure().equals("Empty")) {
+                                boardWay[b][d] = new Cell("Empty", position.getDiagonal() +i, position.getVertical() +i);
+                                if (b == dist.getDiagonal() && d == dist.getVertical()) {
+                                    boardWay[b][d] = new Cell("Bishop", b, d);
+                                    return boardWay;
+                                }
+                            } else {
                                 throw new OccupiedWayException("Occupied Way Exception");
                             }
-                            board[b][d] = new Cell(null, position.getDiagonal() + i, position.getVertical() + i);
-                            if (b == dist.getDiagonal() && d == dist.getVertical()) {
-                                board[b][d] = new Cell("Bishop", b, d);
-                                return board;
-                            }//else ;throw new OccupiedWayException("Occupied Way Exception")
-                        } //else throw new OccupiedWayException("Occupied Way Exception");
-                    }
-                }
-
-                if (position.getDiagonal() < dist.getDiagonal() && position.getVertical() > dist.getVertical()) {
-                    for (int i = 0; i < 8; i++) {
-                        if (position.getDiagonal() + i > 7 || position.getVertical() < 0) {
-                            break;
                         }
-                        int b = position.getDiagonal() + i;
-                        int d = position.getVertical() - i;
-                        board[b][d] = new Cell(null, position.getDiagonal() + i, position.getVertical() - i);
-                        if (b == dist.getDiagonal() && d == dist.getVertical()) {
-                            for (Cell[] cell : board) {
-                                for (Cell cel : cell) {
-                                    if (cel == null) {
-                                        board[b][d] = new Cell("Bishop", b, d);
-                                        return board;
-                                    } else throw new OccupiedWayException("Occupied Way Exception");
+                    }
+
+                    if (position.getDiagonal() < dist.getDiagonal() && position.getVertical() > dist.getVertical()) {
+                        for (int i = 0; i < 8; i++) {
+                            if (position.getDiagonal() + i > 7 || position.getVertical() < 0) {
+                                throw new ImposibleMoveException("Impossible move exception");
+                            }
+                            int b = position.getDiagonal() + i;
+                            int d = position.getVertical() - i;
+                            if (board[b][d].getFigure().equals("Empty")) {
+                                boardWay[b][d] = new Cell("Empty", position.getDiagonal() + i, position.getVertical() - i);
+                                if (b == dist.getDiagonal() && d == dist.getVertical()) {
+                                    boardWay[b][d] = new Cell("Bishop", b, d);
+                                    return boardWay;
                                 }
+                            } else {
+                                throw new OccupiedWayException("Occupied Way Exception");
                             }
                         }
+
                     }
-                }
 
-            }
-
+                }else{throw new ImposibleMoveException("Impossible move exception");}
+            }else{throw new ImposibleMoveException("Impossible move exception");}
             return null;
         }
-        }
 
-        public static void main(String args[]) throws ImposibleMoveException, FigureNotFoundException, OccupiedWayException {
-            Figure bishop = new Bishop(new Cell("Bishop", 1, 1));
-            for (Cell[] cell : bishop.way(new Cell(null, 7, 7))) {
-                for (Cell caellt : cell) {
-                    if (caellt != null) {
-                        System.out.println(caellt.getFigure() + " " + caellt.getDiagonal() + " " + caellt.getVertical());
-                    }
-                }
-            }
-        }
     }
+}
+
+
 
 
 
--- lesson_2/src/test/java/Chess/BishopTest.java
+++ lesson_2/src/test/java/Chess/BishopTest.java
@@ -1,16 +1,72 @@
 package Chess;
 
+import exercise2.*;
+import org.junit.Assert;
 import org.junit.Test;
 
+import static org.hamcrest.core.Is.is;
 import static org.junit.Assert.*;
 
 /**
  * Created by Mur on 02.02.2017.
  */
-public class BishopTest {
-    @Test
-    public void way() throws Exception {
+public class BishopTest extends exercise2.OutTest {
 
+
+    @Test
+    public void wayfirst() throws Exception {
+        Board.Bishop brdFirst=new Board.Bishop(new Cell("Bisop",0,0));
+        for (Cell[] cell : brdFirst.way(new Cell("Empty", 7, 7))) {
+            for (Cell cel : cell) {
+                if (cel != null) {
+                    System.out.println(cel.getFigure() + " " + cel.getDiagonal() + " " + cel.getVertical());
+                }
+            }
+        }
+        Assert.assertThat("Empty 0 0\r\nEmpty 1 1\r\nEmpty 2 2\r\nEmpty 3 3\r\nEmpty 4 4\r\nEmpty 5 5\r\nEmpty 6 6\r\nBishop 7 7\r\n", is(output.toString()));
+    }
+
+
+    @Test
+    public void waySecond() throws Exception {
+        Board.Bishop brdSecond=new Board.Bishop(new Cell("Bisop",5,5));
+        for (Cell[] cell : brdSecond.way(new Cell("Empty", 0, 0))) {
+            for (Cell cel : cell) {
+                if (cel != null) {
+                    System.out.println(cel.getFigure() + " " + cel.getDiagonal() + " " + cel.getVertical());
+                }
+            }
+        }
+        Assert.assertThat("Bishop 0 0\r\nEmpty 1 1\r\nEmpty 2 2\r\nEmpty 3 3\r\nEmpty 4 4\r\nEmpty 5 5\r\n", is(output.toString()));
+    }
+
+
+    @Test
+    public void wayThird() throws Exception {
+        Board.Bishop brdSThird=new Board.Bishop(new Cell("Bisop",6,1));
+        for (Cell[] cell : brdSThird.way(new Cell("Empty", 1, 6))) {
+            for (Cell cel : cell) {
+                if (cel != null) {
+                    System.out.println(cel.getFigure() + " " + cel.getDiagonal() + " " + cel.getVertical());
+                }
+            }
+        }
+        Assert.assertThat( "Bishop 1 6\r\nEmpty 2 5\r\nEmpty 3 4\r\nEmpty 4 3\r\nEmpty 5 2\r\nEmpty 6 1\r\n", is(output.toString()));
+    }
+
+
+    @Test
+    public void wayFourth() throws Exception {
+        Board.Bishop brdFourth=new Board.Bishop(new Cell("Bisop",1,4));
+        for (Cell[] cell : brdFourth.way(new Cell("Empty", 4, 1))) {
+            for (Cell cel : cell) {
+                if (cel != null) {
+                    System.out.println(cel.getFigure() + " " + cel.getDiagonal() + " " + cel.getVertical());
+                }
+            }
+        }
+        Assert.assertThat("Empty 1 4\r\nEmpty 2 3\r\nEmpty 3 2\r\nBishop 4 1\r\n", is(output.toString()));
     }
 
 }
+
