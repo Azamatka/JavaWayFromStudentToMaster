8d077a055dc981a6220d12f3dc5bba9ddd4c4dcf
Azamatka <mmmzzzxz@gmail.com>
2017-03-04 13:33:29 +0500

    IAR

--- lesson_3/src/main/java/exercise1/DelAbuse.java
+++ lesson_3/src/main/java/exercise1/DelAbuse.java
@@ -1,45 +1,43 @@
 package exercise1;
 
 import java.io.*;
+import java.util.Scanner;
+
 
 
 /**
- * Created by Mur on 21.02.2017.
+ * DelAbuse  delete words which includes abuse Array
+ * @since 04.03.2017
+ * @version 1
  */
 public class DelAbuse {
-   public void dropAbuses(InputStream in, OutputStream out, String[] abuse)throws IOException {
-       try (ByteArrayOutputStream result = new ByteArrayOutputStream()) {
-           byte[] buffer = new byte[1024];
-           int length;
-           while ((length = in.read(buffer)) != -1) {
-               result.write(buffer, 0, length);
-           }
-           String res = result.toString("UTF-8");
-           char[] streamText = res.toCharArray();
-           for (String words : abuse) {
-               int count=1;
-               char[]abuses=words.toCharArray();
-               for (int i = 0; i <= streamText.length - abuses.length; i++) {
-                   if (abuses[0] == streamText[i]) {
-                       for (int k = 1; k < abuses.length; k++) {
-                           if (abuses[k] == streamText[k + i]) {
-                               count++;
-                               if (count == abuses.length) {
-                                   for(int d=0;d<abuses.length;d++) {
-                                       streamText[i+d] = '\u0000';
-                                   }
-                               }
-                           }
-                       }
-                   }
-               }
-           }
-               String str = String.valueOf(streamText);
-           System.out.println(str);
-               out.write(str.getBytes("UTF-8"));
-           }
+    /**
+     * dropAbuses  method deletes words which includes abuse Array
+     * @since 04.03.2017
+     * @param in is IputStream
+     * @param out is OutputStream
+     * @param abuse array of words
+     * @version 1
+     */
+    public void dropAbuses(InputStream in, OutputStream out, String[] abuse) throws IOException {
+        Scanner scanner = new Scanner(in);
+        String streamWords = scanner.nextLine();
+        String[] s = streamWords.split(" ");
+        try (BufferedOutputStream outputStream = new BufferedOutputStream(out)) {
+            for (String word : s) {
+                int num = 0;
+                for (String abuses : abuse) {
+                        if (!word.equals(abuses) && ++num == abuse.length) {
+                            outputStream.write(word.getBytes());
+                            outputStream.write(32);
+                        }
+                    }
+                }
+        } catch (Exception e){
+            System.out.println(e.getMessage());
+        }
+    }
+}
 
-       }
-   }
 
 
--- lesson_3/src/test/java/exercise1/DelAbuseTest.java
+++ lesson_3/src/test/java/exercise1/DelAbuseTest.java
@@ -5,21 +5,21 @@ import org.junit.Test;
 
 import java.io.*;
 
-//import static java.lang.System.out;
 import static org.hamcrest.core.Is.is;
-
 /**
- * Created by Mur on 26.02.2017.
+ * DelAbuseTest test which checks if programm  do delete words works correctly
+ * @since 04.03.2017
+ * @version 1
  */
 public class DelAbuseTest {
     @Test
-    public void dropAbuses() throws Exception {
+    public void mustDropAbuses() throws Exception {
             DelAbuse abb=new DelAbuse();
             String[]abbuse={"toDelete", "mustDel", "willDel", "haveToDel", "del"};
-            String streamText="toDelete  hello world mustDel  willDel haveToDel programm del";
+            String streamText="toDelete hello world mustDel willDel haveToDel programm del";
             InputStream stream = new ByteArrayInputStream(streamText.getBytes());
-            String str="  hello world     programm ";
-           OutputStream outt=new ByteArrayOutputStream();
+            String str="hello world programm ";
+              OutputStream outt=new ByteArrayOutputStream();
               abb.dropAbuses(stream,outt,abbuse);
              Assert.assertThat(outt.toString(),is(str));
         }
